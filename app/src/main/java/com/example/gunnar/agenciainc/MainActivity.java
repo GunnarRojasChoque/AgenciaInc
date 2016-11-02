e9cd480a96ba6a825aa904e0a55176824149ae8c
GunnarRojasChoque <gunnarrojas22@gmail.com>
2016-11-01 20:37:33 -0400

    commit Registro de vehiculos IU 2

--- app/src/main/java/com/example/gunnar/agenciainc/MainActivity.java
+++ app/src/main/java/com/example/gunnar/agenciainc/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.gunnar.agenciainc;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText modelo;
-    EditText marca;
-    EditText chasis;
-    EditText anio;
-    EditText fecha;
-    EditText precio;
+    public static EditText modelo;
+    public static EditText marca;
+    public static EditText chasis;
+    public static EditText anio;
+    public static EditText precio;
+    public static TextView fechaNow;
+    public static Button fech;
+    public static Button registro;
+    public static Button cancelar;
+
+    public static int year, month, day;
+    public static final int id_dialog = 0;
 
     private static final String TAG = MainActivity.class.getName();
 
@@ -22,9 +34,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        initCalendar();
         initRegistro();
 
+    }
+
+    private void initCalendar() {
+        final Calendar calendar = Calendar.getInstance();
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
     }
 
     private void initRegistro() {
@@ -32,18 +52,34 @@ public class MainActivity extends AppCompatActivity {
         marca = (EditText) findViewById(R.id.marca);
         chasis = (EditText) findViewById(R.id.chasis);
         anio = (EditText) findViewById(R.id.anio);
-        fecha = (EditText) findViewById(R.id.date);
         precio = (EditText) findViewById(R.id.precio);
+        fechaNow = (TextView) findViewById(R.id.dateNow);
+        registro = (Button) findViewById(R.id.registrar);
+        cancelar = (Button) findViewById(R.id.Cancelar);
+        fech = (Button) findViewById(R.id.date);
 
-        Button registro = (Button) findViewById(R.id.registrar);
-        Button cancelar = (Button) findViewById(R.id.Cancelar);
+        fechaNow.setText(year + "/" + month + "/" + day);
+
+        fechaNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(id_dialog);
+            }
+        });
+
+        fech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(id_dialog);
+            }
+        });
 
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Vehiculo vehiculo = new Vehiculo(modelo.getText().toString(), marca.getText().toString(),
                         chasis.getText().toString(), Integer.parseInt(anio.getText().toString()),
-                        fecha.getText().toString(), Integer.parseInt(precio.getText().toString()));
+                        fechaNow.getText().toString(), Integer.parseInt(precio.getText().toString()));
             }
         });
 
@@ -55,9 +91,28 @@ public class MainActivity extends AppCompatActivity {
                 marca.setText("");
                 chasis.setText("");
                 anio.setText("");
-                fecha.setText("");
+                fechaNow.setText("");
                 precio.setText("");
             }
         });
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        if (id == 0) {
+            return new DatePickerDialog(this, dateSetListener, year, month, day);
+        }
+        return super.onCreateDialog(id);
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            year = i;
+            month = i1;
+            day = i2;
+            fechaNow.setText(year + "/" + month + "/" + day);
+        }
+    };
 }
