fef47e37683ed813f9688114cbac4dd495773c56
willianMaida <willian.maida@gmail.com>
2016-11-04 18:06:59 -0400

    Registro y base de datos del diagnostico del mecanico

---
+++ app/src/main/java/com/example/gunnar/agenciainc/BaseDeDatos/BDMecanico.java
@@ -0,0 +1,58 @@
+package com.example.gunnar.agenciainc.BaseDeDatos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by willian on 04-11-16.
+ */
+
+public class BDMecanico extends SQLiteOpenHelper {
+    private static final String TAG = BDMecanico.class.getSimpleName();
+
+    public static final String TABLE_MECANICO_IMPORTADORA = "Mecanico";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NOMBRES = "nombres";
+    public static final String COLUMN_APELLIDOS = "apellidos";
+    public static final String COLUMN_CELULAR = "celular";
+    public static final String COLUMN_CI = "ci";
+    public static final String COLUMN_CHASIS = "chasis";
+    public static final String COLUMN_DIAGNOSTICO = "diagnostico";
+    public static final String COLUMN_FECHA = "fecha";
+
+
+
+    private static final String DATABASE_MECANICO = "BaseDatosMecanico.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Setencia SQL para crear la base de datos
+    private static final String DATABASE_CREATE = "CREATE TABLE "
+            + TABLE_MECANICO_IMPORTADORA + " (" + COLUMN_ID + " INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+            + COLUMN_NOMBRES + " TEXT NOT NULL, "
+            + COLUMN_APELLIDOS + " TEXT NOT NULL, "
+            + COLUMN_CELULAR + " INT NOT NULL, "
+            + COLUMN_CI + " INT NOT NULL, "
+            + COLUMN_CHASIS + " TEXT NOT NULL, "
+            + COLUMN_DIAGNOSTICO + " TEXT NOT NULL, "
+            + COLUMN_FECHA + " TEXT NOT NULL);";
+
+
+    public BDMecanico(Context context){
+        super(context, DATABASE_MECANICO, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        //Ejecuta la sentencia SQL y crea la base de datos
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, "Upgradind database from version " + oldVersion + "to" + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MECANICO_IMPORTADORA);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/gunnar/agenciainc/Mains/MainActivity.java
+++ app/src/main/java/com/example/gunnar/agenciainc/Mains/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.gunnar.agenciainc.Mains;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.gunnar.agenciainc.BaseDeDatos.BDMecanico;
 import com.example.gunnar.agenciainc.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private Button registroCL;
     private static Context context;
 
+    public SQLiteDatabase database;
+
     private static final String TAG = MainActivity.class.getName();
 
     @Override
@@ -22,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         context = this;
+
+        BDMecanico bdMecanico = new BDMecanico(this);
+        database = bdMecanico.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(BDMecanico.COLUMN_NOMBRES, "asdas");
+        contentValues.put(BDMecanico.COLUMN_APELLIDOS, "asdas");
+        contentValues.put(BDMecanico.COLUMN_CELULAR, "asdas");
+        contentValues.put(BDMecanico.COLUMN_CI, "asdas");
+        contentValues.put(BDMecanico.COLUMN_FECHA_NAC, "asdas");
+        contentValues.put(BDMecanico.COLUMN_CORREO, "asdasas");
+        contentValues.put(BDMecanico.COLUMN_GENERO, "asasdasas");
+
+        long row = database.insert(BDMecanico.TABLE_MECANICO_IMPORTADORA, null, contentValues);
+
+        Log.i(TAG, "onCreate: id = " + row);
+
+
+
+
+
 //Prueba
         initView();
     }
